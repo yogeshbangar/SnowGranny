622f598494a413868768915d45c9a9a9224e56c5
Hututu Games <yogeshbangar@gmail.com>
2020-10-28 11:43:07 +0530

    v 1.1

---
+++ Assets/code/M.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public static class M
+{
+	public static bool setSound = true;
+	public static bool isAds = true;
+	public static int []STARS = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
+	public static int []UPGEDE = new int[]{0,0,0,0,0};
+	public static int []GRANNYCOST = new int[]{0,5999,8999,7999,9999,6999};
+	public static string[] NAME = new string[] { "HOMEBODY", "IMPOVERISHED", "LANKY", "MORBIDLY OBESE", "EUROPEAN", "MIDGET" };
+	public static int UNLOCKLEVEL = 1;
+	public static int COINS = 1;
+
+	public static int GameScreen;
+	public  const int GAMEMENU = 0;
+	public  const int GAMESHOP = 1;
+	public  const int GAMELEVEL = 2;
+	public  const int GAMEEXIT = 3;
+	public  const int GAMEPLAY = 4;
+	public  const int GAMEOVER = 5;
+	public  const int GAMEWIN = 6;
+	public  const int GAMEPAUSE = 7;
+	public  const int GAMESTART = 8;
+	public  const int GAMEIAP = 9;
+	public  const int GAMEREVIVE = 10;
+
+	public static void Save ()
+	{
+		
+		PlayerPrefs.SetInt ("a", setSound ? 1 :0);
+		PlayerPrefs.SetInt ("b", isAds ? 1 :0);
+
+		for (int i = 0; i < STARS.GetLength(0); i++) 
+			PlayerPrefs.SetInt ("c"+i, STARS[i]);
+		for (int i = 0; i < UPGEDE.GetLength(0); i++)
+			PlayerPrefs.SetInt ("d"+i, UPGEDE[i]);
+		for (int i = 0; i < GRANNYCOST.GetLength(0); i++)
+			PlayerPrefs.SetInt ("e"+i, GRANNYCOST[i]);
+		PlayerPrefs.SetInt ("f", UNLOCKLEVEL);
+		PlayerPrefs.SetInt ("g", COINS);
+
+		Debug.Log ("Save - >"+UNLOCKLEVEL);
+
+	}
+
+	public static void Open ()
+	{
+		setSound = PlayerPrefs.GetInt ("a", 1) == 1;
+		isAds = PlayerPrefs.GetInt ("b", 1) ==1;
+
+		for (int i = 0; i < STARS.GetLength(0); i++) 
+			STARS[i] = PlayerPrefs.GetInt ("c"+i, STARS[i]);
+		for (int i = 0; i < UPGEDE.GetLength(0); i++)
+			UPGEDE[i] = PlayerPrefs.GetInt ("d"+i, UPGEDE[i]);
+		for (int i = 0; i < GRANNYCOST.GetLength(0); i++)
+			GRANNYCOST[i] = PlayerPrefs.GetInt ("e"+i, GRANNYCOST[i]);
+		UNLOCKLEVEL = PlayerPrefs.GetInt ("f", UNLOCKLEVEL);
+		COINS = PlayerPrefs.GetInt ("g", COINS);
+		Debug.Log ("Open - >"+UNLOCKLEVEL);
+	}
+}
---
+++ Assets/code/Menu.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Menu : MonoBehaviour
+{
+    Transform mTrans_Granny;
+    Transform mTrans_GrannyBig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTrans_Granny = GameObject.Find("Granny").transform;
+        mTrans_GrannyBig = GameObject.Find("GrannyBig").transform;
+        setGranny(M.NAME[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)||((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+            {
+                ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            }
+            if (Physics.Raycast(ray, out hit))
+            {
+                setGranny(hit.collider.name);
+            }
+            
+            
+        }
+    }
+    void setGranny(string str) {
+        for (int i = 0; i < mTrans_GrannyBig.childCount; i++)
+        {
+            if(mTrans_GrannyBig.GetChild(i).name == str)
+            {
+                for (int j = 0; j < mTrans_GrannyBig.childCount; j++)
+                {
+                    mTrans_GrannyBig.GetChild(j).gameObject.SetActive(false);
+                }
+                mTrans_GrannyBig.GetChild(i).gameObject.SetActive(true);
+                setcost(i);
+            }
+        }
+    }
+    void setcost(int no)
+    {
+        Transform shopMid = transform.Find("Shop").Find("Middile");
+        if (M.GRANNYCOST[no] > 0)
+        {
+            shopMid.Find("cost").Find("Text").GetComponent<Text>().text = "    " + M.GRANNYCOST[no];
+            shopMid.Find("cost").Find("Image").gameObject.SetActive(true);
+        }
+        else
+        {
+            shopMid.Find("cost").Find("Text").GetComponent<Text>().text = "PLAY";
+            shopMid.Find("cost").Find("Image").gameObject.SetActive(false);
+        }
+        shopMid.Find("Name").GetComponent<Text>().text = ""+ M.NAME[no];
+    }
+    public void onClick(string val) {
+        switch (val) {
+            case "selNoti":
+                break;
+            case "selCoin":
+                break;
+            case "selLeader":
+                break;
+            case "selGranny":
+                setScreen(1);
+                break;
+            case "selSki":
+                break;
+            case "selShop":
+                break;
+        }
+    }
+    void setScreen(int scr) {
+        for (int i = 0; i < transform.childCount;i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+        transform.GetChild(scr).gameObject.SetActive(true);
+    }
+}
